9240fbd454030410d234ecc757799a432fc62a24
Satrio Coman Azizi <luminalwww@gmail.com>
2024-01-30 23:53:28 +0700

    added profile image

--- src/components/ui/shared/Topbar.tsx
+++ src/components/ui/shared/Topbar.tsx
@@ -2,13 +2,15 @@ import {useEffect} from "react";
 import { Link , useNavigate } from "react-router-dom";
 import { Button } from "../button";
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutation";
+import { useUserContext } from "@/context/AuthContext";
 
-const navigate = useNavigate
 const Topbar = () => {
+    const navigate = useNavigate
     const { mutate: signOut, isSuccess } = useSignOutAccount()
+    const  { user } = useUserContext();
     
     useEffect(() => {
-      if (isSuccess) navigate()
+      if (isSuccess) navigate(0)
     
     }, [isSuccess])
     
@@ -28,6 +30,9 @@ const Topbar = () => {
             <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
                 <img src="/assets/icons/logout.svg" alt="logout" />
             </Button>
+            <Link to={'/profile/${user.id}'} className="flex-center gap-3">
+              <img src={user.imageUrl || '/assets/images/profile-placeholder.svg'} alt="" />
+            </Link> 
 
         </div>
       </div>
