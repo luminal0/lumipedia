c4ddda934a7d2d224e70c6d726daa2929822d457
Satrio Coman Azizi <luminalwww@gmail.com>
2024-02-08 23:36:16 +0700

    post form update and file upload logic added

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -11,3 +11,10 @@ export const signUpValidation = z.object({
     email: z.string().email(),
     password: z.string().min(6, {message: 'Password must be at least 6 characters long'}),
   });
+
+  export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string()
+  });
