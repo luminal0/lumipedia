bf221ff2f16bbe6c7120510ac313f31ced7d3ef8
Satrio Coman Azizi <luminalwww@gmail.com>
2024-02-12 23:50:17 +0700

    updated post, save and liked added

--- src/components/ui/shared/PostCard.tsx
+++ src/components/ui/shared/PostCard.tsx
@@ -1,12 +1,19 @@
+import { useUserContext } from "@/context/AuthContext";
 import { formatDate } from "@/lib/utils";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type PostCardProps = {
   post: Models.Document;
 };
 
 const PostCard = ({ post }: PostCardProps) => {
+const { user }  = useUserContext();
+
+console.log(post)
+
+  if(!post.creator) return;
   return (
     <div className="post-card">
       <div className="flex-between">
@@ -37,10 +44,28 @@ const PostCard = ({ post }: PostCardProps) => {
             </p>
           </div>
         </div>
-        <Link to={`/update-post/${post.$id}`}>
+        <Link to={`/update-post/${post.$id}`}
+        className={`${user.id !== post.creator.$id && "hidden"}`}
+        >
           <img src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
         </Link>
       </div>
+      <Link to ={`/posts/${post.$id}`}>
+        <div className="small-medium lg:base-medium py-5">
+              <p>{post.caption}</p>
+              <ul className="flex gap-1 mt-2">
+                {post.tags.map((tag: string) => (
+                  <li key={tag} className="text-light-3">
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
+        </div>
+        <img src={post.imageUrl || 'assets/icons/profile-placeholder.svg'} 
+        className="post-card_img" alt="post image" />
+      </Link>
+
+      <PostStats post={post} userId={user.id} />
     </div>
   );
 };
--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -5,7 +5,7 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createPost, createUserAccount, getRecentPost, signInAccount, signOutAccount } from "../appwrite/api";
+import { createPost, createUserAccount, deleteSavedPost, getRecentPost, likePost, savePost, signInAccount, signOutAccount } from "../appwrite/api";
 import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
@@ -45,5 +45,67 @@ export const useGetRecentPosts = () => {
   return useQuery({
     queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
     queryFn: getRecentPost,
+  })
+}
+
+export const useLikePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({postId, likesArray} : { postId: string, likesArray: string[]}) =>
+    likePost(postId, likesArray),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+      })
+    }
+  })
+}
+
+export const useSavePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({postId, userId} : { postId: string, userId: string}) =>
+    savePost(postId, userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+      })
+    }
+  })
+}
+
+export const useDeleteSavePost = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POSTS]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+      })
+    }
   })
 }
