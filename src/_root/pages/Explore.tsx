cf0d0e0950a9a0b36fb0a7370592ea200fe4683d
Satrio Coman Azizi <luminalwww@gmail.com>
2024-02-16 22:48:14 +0700

    working search and top posts

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -5,16 +5,25 @@ import {
   useGetPosts,
   useSearchPosts,
 } from "@/lib/react-query/queriesAndMutation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useDebounce from "../../../hooks/useDebounce";
 import Loader from "@/components/ui/shared/loader";
+import { useInView } from "react-intersection-observer";}
 
 const Explore = () => {
+  const { ref, inView } = useInView();
+
   const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
   const [searchValue, setSearchValue] = useState("");
   const debouncedValue = useDebounce(searchValue, 500);
   const { data: searchedPosts, isFetching: isSearchFetching } =
     useSearchPosts(debouncedValue);
+
+    useEffect(() => {
+      if (inView && !searchValue) {
+        fetchNextPage();
+      }
+    }, [inView,searchValue])
 
   if (!posts) {
     return (
@@ -27,7 +36,7 @@ const Explore = () => {
   const shouldShowSearchResults = searchValue !== "";
   const shouldShowPosts =
     !shouldShowSearchResults &&
-    posts.pages.every((item) => item.documents.lenght === 0);
+    posts.pages.every((item) => item.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -75,6 +84,12 @@ const Explore = () => {
           ))
         )}
       </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
